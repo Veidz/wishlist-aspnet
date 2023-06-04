1abb6281927c23a4da1e4b5be4f62cf41ae55e70
Veidz <joaovictorveidz@gmail.com>
2023-06-04 20:46:44 -0300

    feat: implement GetAllAsync ProductController

---
+++ Wishlist/Wishlist.API/Controllers/ProductController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks;
+using Wishlist.Application.Contracts;
+using Wishlist.Domain.Entities;
+
+namespace Wishlist.API.Controllers
+{
+    [ApiController]
+    [Route("/products")]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductService ProductService;
+
+        public ProductController(IProductService productService)
+        {
+            ProductService = productService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllAsync()
+        {
+            try
+            {
+                IEnumerable<Product> products = await ProductService.GetAllAsync();
+                return StatusCode((int)HttpStatusCode.OK, products);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError, ex.Message);
+            }
+        }
+    }
+}
