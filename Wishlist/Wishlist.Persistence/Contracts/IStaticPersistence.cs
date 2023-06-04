fad6cf9fe5160eaf26dc4663bbb0d0ac24e54278
Veidz <joaovictorveidz@gmail.com>
2023-06-04 18:17:36 -0300

    feat: create StaticPersistence with GetAllAsync method

---
+++ Wishlist/Wishlist.Persistence/Contracts/IStaticPersistence.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Wishlist.Persistence.Contracts
+{
+    public interface IStaticPersistence<D> where D : class
+    {
+        Task<IEnumerable<D>> GetAllAsync();
+    }
+}
---
+++ Wishlist/Wishlist.Persistence/Persistence/StaticPersistence.cs
@@ -0,0 +1,18 @@
+﻿using MongoDB.Driver;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Wishlist.Persistence.Contexts;
+using Wishlist.Persistence.Contracts;
+
+namespace Wishlist.Persistence.Persistence
+{
+    public class StaticPersistence<D> : BasePersistence<D>, IStaticPersistence<D> where D : class
+    {
+        public StaticPersistence(IMongoDbContext mongoDbContext) : base(mongoDbContext) { }
+
+        public async Task<IEnumerable<D>> GetAllAsync()
+        {
+            return await MongoCollection.Find(Builders<D>.Filter.Empty).ToListAsync();
+        }
+    }
+}
