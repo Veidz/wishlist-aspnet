caed357fd0d8b1617d1e403a7896ee98155b0b65
Veidz <joaovictorveidz@gmail.com>
2023-06-04 18:17:25 -0300

    feat: create BasePersistence

---
+++ Wishlist/Wishlist.Persistence/Persistence/BasePersistence.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Driver;
+using Wishlist.Persistence.Contexts;
+
+namespace Wishlist.Persistence.Persistence
+{
+    public class BasePersistence<D> where D : class
+    {
+        protected IMongoDbContext MongoDbContext;
+        protected IMongoCollection<D> MongoCollection;
+
+        public BasePersistence(IMongoDbContext mongoDbContext)
+        {
+            MongoDbContext = mongoDbContext;
+            MongoCollection = mongoDbContext.GetCollection<D>();
+        }
+    }
+}
