64cab2a7de5de38d62e9a60613a60acd43ebdf8a
Veidz <joaovictorveidz@gmail.com>
2023-06-04 20:46:26 -0300

    feat: implement GetAllAsync ProductService

---
+++ Wishlist/Wishlist.Application/Contracts/IProductService.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Wishlist.Domain.Entities;
+
+namespace Wishlist.Application.Contracts
+{
+    public interface IProductService
+    {
+        Task<IEnumerable<Product>> GetAllAsync();
+    }
+}
---
+++ Wishlist/Wishlist.Application/Services/ProductService.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Wishlist.Application.Contracts;
+using Wishlist.Domain.Entities;
+using Wishlist.Persistence.Contracts;
+
+namespace Wishlist.Application.Services
+{
+    public class ProductService : IProductService
+    {
+        private readonly IStaticPersistence<Product> ProductStaticPersistence;
+
+        public ProductService(IStaticPersistence<Product> productStaticPersistence)
+        {
+            ProductStaticPersistence = productStaticPersistence;
+        }
+
+        public async Task<IEnumerable<Product>> GetAllAsync()
+        {
+            IEnumerable<Product> products = await ProductStaticPersistence.GetAllAsync();
+            return products;
+        }
+    }
+}
