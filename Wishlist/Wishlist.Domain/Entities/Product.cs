1098e59ce78daa50fb9e492b959b38e9baec7a4a
Veidz <joaovictorveidz@gmail.com>
2023-06-04 20:46:10 -0300

    feat: create Product domain

---
+++ Wishlist/Wishlist.Domain/Entities/Product.cs
@@ -0,0 +1,23 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Wishlist.Domain.Entities
+{
+    public class Product
+    {
+        [BsonId]
+        public ObjectId Id { get; }
+
+        [BsonRequired]
+        [BsonRepresentation(BsonType.String)]
+        public string Name { get; set; } = string.Empty;
+
+        [BsonRequired]
+        [BsonRepresentation(BsonType.String)]
+        public string ImageUrl { get; set; } = string.Empty;
+
+        [BsonRequired]
+        [BsonRepresentation(BsonType.String)]
+        public string ProductUrl { get; set; } = string.Empty;
+    }
+}
